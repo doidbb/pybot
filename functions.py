dd7d326cf5d77094302f461cdf214d479a5c9ac9
doidbb <hfitz83@gmail.com>
2014-08-06 22:34:02 +0100

    added untested functions code. more work will be needed

--- functions.py
+++ functions.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-
-def parseurl():
-
-def lastfm():
-
-def log():
-
-
+import re, requests
+from bs4 import Beautifulsoup
+def parseurl(url):
+    ourInfo = ""
+    imgDL1 = BeautifulSoup(url)
+    imgDL = requests.get(url)
+    imgInfo = imgDL.headers['content-type']
+    urltitle = imgDL1.title.string
+    return urltitle + imgInfo
