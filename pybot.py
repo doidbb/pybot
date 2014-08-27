87f19bb30b5b309aff7873ab20da3a7e0bb35890
doidbb <hfitz83@gmail.com>
2014-08-28 00:14:50 +0100

    added joining, working on parsing like the type seen in sadbot

--- pybot.py
+++ pybot.py
@@ -5,27 +5,18 @@ import socket
 network = "irc.rizon.net"
 port = 6660
 username = "sadpy"
-ident = username
-nick = username
-ident1 = "USER " + ident + " 0 1 :" + username
-ident2 = "NICK " + nick
 
-def init(servname, servport):
-    global s
-    s = socket.socket(
-        socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((servname, servport)) #server name and port
-    s.send('NICK %s\n' %nick)
-    s.send('USER %s 0 1 : %s\n' %(nick, nick))
-    mainloop()
-def mainloop():
-    while 1:
-        global data
-        data = s.recv(1024)
-        print data
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((network, port)) #server name and port
 
-#def parse():
+s.send('NICK sadpy\r\n')
+s.send('USER sadpy sadpy sadpy :sadpy sadpy\r\n')
+s.send('JOIN #sadbot-dev \r\n')
 
-
-if __name__ == '__main__':
-    init(network,port)
+while 1:
+  global data
+  data = s.recv(1024)
+  print data
+  nick = data.split('!')[0]
+  nick = nick.replace(":", "")
+  print nick
